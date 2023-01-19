b31106481dbcd211d6f41cac70b67818e2be79e3
Marcin Gnap <258953@student.pwr.edu.pl>
2023-01-19 18:39:35 +0100

    clinet panel - warranty claim button added

--- src/main/java/Forms/ClientPanel.java
+++ src/main/java/Forms/ClientPanel.java
@@ -6,4 +6,5 @@ public class ClientPanel {
     private JButton configureCarButton;
     private JButton viewAvailableCarsButton;
     private JScrollPane orderdCarsList;
+    private JButton warrantyClaimButton;
 }
