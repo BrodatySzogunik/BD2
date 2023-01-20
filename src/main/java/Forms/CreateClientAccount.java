55d4ca17c34342cd81103c3c5041883a82306f3c
Marcin Gnap <258953@student.pwr.edu.pl>
2023-01-20 13:44:39 +0100

    added CreateClientAccount.form

--- src/main/java/Forms/CreateClientAccount.java
+++ src/main/java/Forms/CreateClientAccount.java
@@ -5,4 +5,13 @@ import javax.swing.*;
 public class CreateClientAccount {
     private JButton returnButton;
     private JButton proccedButton;
+    private JTextField first_name_text;
+    private JTextField last_name_text;
+    private JTextField birth_date_text;
+    private JTextField pesel_text;
+    private JTextField postal_code_text;
+    private JTextField city_text;
+    private JTextField street_text;
+    private JTextField building_number_text;
+    private JPasswordField password_pass_text;
 }
