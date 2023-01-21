6c6ab3e28564936559c7fc2aeba2e2932b18033e
Marcin Gnap <258953@student.pwr.edu.pl>
2023-01-21 18:59:34 +0100

    AvailableCarsService

---
+++ src/main/java/Services/AvailableCarsService.java
@@ -0,0 +1,17 @@
+package Services;
+
+import DbModels.*;
+
+import java.util.ArrayList;
+
+public class AvailableCarsService {
+    public static ArrayList<AvailableCar> getAvailableCars() {
+        DatabaseConnector db = new DatabaseConnector();
+        try{
+            return ResultSetConverter.resultSetToArrayList(db.getAvailableCars(), AvailableCar.class);
+        }catch (Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+}
