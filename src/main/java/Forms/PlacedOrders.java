0c93ad50569a2df473be0529b132443c50f8e130
Marcin Gnap <258953@student.pwr.edu.pl>
2023-01-21 01:00:58 +0100

    i hope all forms done by now

---
+++ src/main/java/Forms/PlacedOrders.java
@@ -0,0 +1,10 @@
+package Forms;
+
+import javax.swing.*;
+
+public class PlacedOrders {
+    private JPanel panel1;
+    private JButton returnButton;
+    private JButton changeStatusButton;
+    private JComboBox orderStatus;
+}
