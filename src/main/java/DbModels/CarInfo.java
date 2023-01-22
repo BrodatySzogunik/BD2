64f3264f6527834fb1e09eca36c192d9f29da932
Marcin Gnap <258953@student.pwr.edu.pl>
2023-01-22 14:44:27 +0100

    Diego ma małego

--- src/main/java/DbModels/CarInfo.java
+++ src/main/java/DbModels/CarInfo.java
@@ -7,15 +7,9 @@ public class CarInfo {
     public String capacity;
     public String fuel;
     public String horse_power;
-    public String power;
-    public String torque;
     public String gearbox_type;
     public String gears_number;
     public String diameter;
-    public String ET;
-    public String producer;
-    public String weight;
-    public String width;
     public String color_name;
 
     public CarInfo() {
@@ -23,9 +17,9 @@ public class CarInfo {
 
     @Override
     public String toString(){
-        return ("Brand: "+this.brand_name+" , Model: "+ this.model_name+" , Body type: "+this.body_type+" , Engine capacity: "+this.capacity+" , Fuel: "+this.fuel+" , HP: "+this.horse_power+" , Power: "+this.power+" , Torque: "+this.torque+" , Gearbox type: "+this.gearbox_type+" , Gears number: "+this.gears_number+" , Wheels diameter: "+this.diameter+" , Wheels ET: "+this.ET+" , Wheels producer: "+this.producer+" , Wheels weight: "+this.weight+" , Wheels width: "+this.width+" , Color: "+this.color_name);
+        return ("Brand: "+this.brand_name+" , Model: "+ this.model_name+" , Body type: "+this.body_type+" , Engine capacity: "+this.capacity+" , Fuel: "+this.fuel+" , HP: "+this.horse_power+" , Gearbox type: "+this.gearbox_type+" , Gears number: "+this.gears_number+" , Wheels diameter: "+this.diameter+" , Color: "+this.color_name);
     }
-    public CarInfo(String brand_name, String model_name, String body_type, String capacity, String fuel, String horse_power, String power, String torque, String gearbox_type, String gears_number, String diameter, String ET, String producer, String weight, String width, String color_name) {
+    public CarInfo(String brand_name, String model_name, String body_type, String capacity, String fuel, String horse_power, String gearbox_type, String gears_number, String diameter, String color_name) {
 
         this.brand_name = brand_name;
         this.model_name = model_name;
@@ -33,15 +27,9 @@ public class CarInfo {
         this.capacity = capacity;
         this.fuel = fuel;
         this.horse_power = horse_power;
-        this.power = power;
-        this.torque = torque;
         this.gearbox_type = gearbox_type;
         this.gears_number = gears_number;
         this.diameter = diameter;
-        this.ET = ET;
-        this.producer = producer;
-        this.weight = weight;
-        this.width = width;
         this.color_name = color_name;
     }
 
